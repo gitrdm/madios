d989feff2dac13264949f996eb2d3255f1b6028b
gitrdm <49376741+gitrdm@users.noreply.github.com>
2025-06-06 13:51:01 -0500

    testing trimspace for segfault

---
+++ tests/test_trimspaces_standalone.cpp
@@ -0,0 +1,11 @@
+#include "../src/utils/MiscUtils.h"
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string s = "  a  ";
+    std::cout << "Input: '" << s << "'\n";
+    std::string trimmed = trimSpaces(s);
+    std::cout << "Output: '" << trimmed << "'\n";
+    return 0;
+}
